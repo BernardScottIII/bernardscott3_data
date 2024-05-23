62c5c993e3ab2ee24ccb393065708781baf76a4f
BernardScottIII <106618915+BernardScottIII@users.noreply.github.com>
2024-05-22 21:12:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8000;
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
+
+var jsonServer = require('json-server')
+var server = jsonServer.create()
+var router = jsonServer.router('db.json')
+var middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, function () {
+  console.log('JSON Server is running')
+})
